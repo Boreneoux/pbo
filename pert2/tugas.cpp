9096efeaa08b4a2d4c9f630e774caef4e1f3f548
Boreneoux <ichlasul.ap@gmail.com>
2021-09-30 15:29:54 +0700

    tugas week 2

--- pert2/tugas.cpp
+++ pert2/tugas.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
+#include <iostream> 
 
 using namespace std;
 
-int main(){
+int main(){ //function utama pada program ini
 
-    int tahun = 2021;
-    int lahir;
-    int umur;
+    int tahun = 2021; //deklarasi variabel tahun dengan tipedata integer dan set variabel tersebut dengan 2021 atau tahun sekarang
+    int lahir; //deklarasi variabel lahir dengan tipe data integer tapi tanpa di set karena untuk disesuaikan tahun lahir si user
+    int umur; // deklarasi vairabel umur untuk hasil umur si user nanti
 
-    cout<<"Masukkan tahun lahir anda : "; cin >> lahir;
-    umur = tahun - lahir;
-    cout<<"Berarti usia anda " << umur << " Tahun";
+    cout<<"Masukkan tahun lahir anda : "; cin >> lahir; //mengeluarkan teks menanyakan tahun lahir si user dan menyuruh user menginput tahunnya lalu mengambil data tersebut dengan mengubah value dari si variabel lahir 
+    umur = tahun - lahir; // mengubah value si variabel umur dengan melakukan operasi pengurangan tahun yang sudah kita deklarasi di program dengan si tahun yang di input oleh user
+    cout<<"Berarti usia anda " << umur << " Tahun"; //mengeluarkan teks dengan value umur user dengan hasil operasi yang sudah dilakukan di line 11
 
-return 0;
+return 0; // end of the program 
 
 }
