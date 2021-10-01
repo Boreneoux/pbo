830c1e7d1f9ade6ea015b3a0428fc01d3bf37d05
Boreneoux <ichlasul.ap@gmail.com>
2021-10-01 07:56:22 +0700

    tugas week 2

--- pert2/tugas.cpp
+++ pert2/tugas.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+/* yang saya tangkap dari soal tugas ini adalah mengeluarkan output "Berarti usia anda 21 tahun",
+yang menurut saya itu adalah fungsi untuk menebak umur atau AgeGuesser
+maka yang saya lakukan di program ini adalah menebak umur user di tahun 2021 jadi si user hanya perlu
+memaukan input tahun lahir si user. untuk mendapatkan output yang sama persis di soal (21 tahun) 
+berarti si user hanya perlu memasukkan tahun kelahiran 2000 maka dengan otomatis si program akan mengurangi tahun ini
+dengan tahun kelahiran user (ex : 2021 - 2000 = 21 Tahun)
+tetapi di program ini saya membiarkan user untuk dapat memasukkan tahun kelahirannya sendiri agar 
+mendapatkan output yang berbeda2 sesuai siapa yang menjalankan program ini*/
+
 int main(){ //function utama pada program ini
 
     int tahun = 2021; //deklarasi variabel tahun dengan tipedata integer dan set variabel tersebut dengan 2021 atau tahun sekarang
